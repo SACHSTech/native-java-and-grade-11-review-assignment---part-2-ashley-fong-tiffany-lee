8ea148d2247387020b8d8e65982f0a68ca5a4d79
Tiffany Lee <>
2021-05-06 19:06:43 +0000

    methods 1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,1 +1,26 @@
 package gr11review.part2;
+
+import gr11review.part2.Utility.*;
+import java.io.*;
+import java.util.Arrays;
+
+/* 
+* Grade 11 Java Review Part 2
+* @author: T. Lee
+*
+*/
+
+public class Main{
+  public static void main(String[] args) throws IOException{
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+  
+    // Methods 1
+    System.out.println(Utility.zipZap("zipXzap"));
+
+    // FileIO = Read 1
+    System.out.println(Utility.longestWord("src/gr11review/part2/words.txt"));
+
+
+  }
+  
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -3,7 +3,24 @@ import java.io.*;
 
 public class Utility {
 
-    
+public static String zipZap(String str){
+  
+ String output = ""; 
 
+  for (int i = 0; i < str.length(); i++) {
+      if (i + 2 < str.length() && str.charAt(i) == 'z' && str.charAt(i + 2) == 'p') {
+        output += "zp";
+        i += 2;
+      }
+      else {
+        output += str.charAt(i);
+      }
+    }
+
+    // Return output string
+    return output;
     
+}
+
+
 }
